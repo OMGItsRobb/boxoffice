ab3f31b727abaab2bbe860b06cd4f9467bc5a251
Nolan <ntronowicz@gmail.com>
2021-10-15 22:04:50 -0400

    Added Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-    return <div>Hi!</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home page
+      </Route>
+
+      <Route exaxt path="/starred">
+        This is starr3d content
+      </Route>
+
+      <Route>
+        <h1>404 Page Not Found</h1>
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
