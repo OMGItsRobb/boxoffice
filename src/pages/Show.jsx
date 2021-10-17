afb8f2af73bf5249af3b83c1ff7cb55e8ba2a1d2
Nolan <ntronowicz@gmail.com>
2021-10-17 12:02:14 -0400

    Added loading and error mssg

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -5,16 +5,40 @@ import { apiGet } from '../misc/config';
 function Show() {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed=seasons&embed[]=cast`).then(results => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
       setShow(results);
     });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
     return () => {
-      console.log();
+      isMounted = false;
     };
   }, [id]);
-  console.log(show);
+
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error Occured: {error}</div>;
+  }
 
   return <div>This is show page!</div>;
 }
