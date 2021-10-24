a9f5e3cdf3b28dec9125c365544ca4bde9ca2fae
Nolan <ntronowicz@gmail.com>
2021-10-23 21:54:37 -0400

    Updates

--- src/misc/config.jsx
+++
@@ -1,6 +0,0 @@
-const API_BASE_URL = 'https://api.tvmaze.com';
-
-export async function apiGet(querystring) {
-  const res = await fetch(`${API_BASE_URL}${querystring}`).then(r => r.json());
-  return res;
-}
